9ffbb1c8cb1fb4670495e8b9061617d6501eea5f
Chao <zhangchao@hsort.com>
2022-12-09 18:34:53 +0800

    1

---
+++ MySqlToCsv/CsvWriter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlToCsv
+{
+    public static class CsvWriter
+    {
+        #region Constants
+        private const char DefaultDelimiter = ',';
+        private const char DefaultQuote = '"';
+        private const char DefaultEscape = '"';
+        #endregion
+
+        #region Write
+
+        public static TextWriter WriteRows(IEnumerable<string[]> rows, params string[] cols)
+        {
+            TextWriter sw = new StringWriter();
+            Write(sw, cols);
+            Write(sw, rows);
+            return sw;
+        }
+
+        public static void Write(TextWriter writer, IEnumerable<string[]> rows)
+        {
+            foreach (var row in rows)
+            {
+                Write(writer, row, DefaultDelimiter);
+            }
+        }
+
+        public static void Write(TextWriter writer, IEnumerable<string> row)
+        {
+            Write(writer, row, DefaultDelimiter);
+        }
+
+        public static void Write(TextWriter writer, IEnumerable<string> row, char delimiter)
+        {
+            bool start = false;
+            foreach (string str in row)
+            {
+                if (start == true)
+                    writer.Write(delimiter);
+                else
+                    start = true;
+                writer.Write(CsvString(str, delimiter));
+            }
+            writer.WriteLine();
+        }
+
+        public static string CsvString(string str, char delimiter)
+        {
+            if (string.IsNullOrEmpty(str)) return string.Empty;
+            if (str.Contains(delimiter.ToString()) || str.Contains(DefaultQuote.ToString()) || str.Contains(@"\r") || str.Contains(@"\n"))
+            {
+                var sb = new StringBuilder();
+                sb.Append(DefaultEscape);
+                foreach (char c in str)
+                {
+                    if (c == DefaultQuote) sb.Append(DefaultQuote);
+                    sb.Append(c);
+                }
+                sb.Append(DefaultEscape);
+                return sb.ToString();
+            }
+            return str;
+        }
+
+        #endregion
+    }
+}
---
+++ MySqlToCsv/Program.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Dapper;
+using System.Data;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace MySqlToCsv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("input export sql:");
+                var sql = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(sql)) continue;
+                var fileName = ExportToCsvBySql(sql);
+                Console.WriteLine("export success to " + fileName);
+            }
+        }
+
+        static string ExportToCsvBySql(string sql)
+        {
+            var constr = ConfigurationManager.AppSettings["constr"];
+
+            using (var db = new MySqlConnection(constr))
+            {
+                using (var reader = db.ExecuteReader(sql))
+                {
+                    // use reader here
+                    return ToCsv(reader);
+                }
+            }
+        }
+
+        public static string ToCsv(IDataReader reader)
+        {
+            var filePath = DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(string.Join(",", Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList()));
+                while (reader.Read())
+                {
+                    var cells = Enumerable.Range(0, reader.FieldCount)
+                        .Select(reader.GetValue)
+                        .Select(t =>
+                        {
+                            if (t == null) return null;
+                            return CsvWriter.CsvString(t.ToString(), ',');
+                        });
+
+                    writer.WriteLine(string.Join(",", cells));
+                }
+                writer.Flush();
+            }
+            return filePath;
+        }
+    }
+}
